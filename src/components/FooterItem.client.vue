66f07c346a9eebf27805e02a25909565d28fd4de
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-05 21:59:46 +0800

    feat: add icp information

--- src/components/FooterItem.client.vue
+++ src/components/FooterItem.client.vue
@@ -2,16 +2,21 @@
   <footer class="mt-auto w-full py-12">
     <div class="mx-auto max-w-7xl">
       <div
-        class="flex flex-col justify-center text-lg leading-10 text-neutral-500 dark:text-neutral-500"
+        class="flex flex-col justify-center text-lg leading-8 text-neutral-500 dark:text-neutral-500"
       >
         <div class="w-full text-center">
           <span>尝试链接几乎所有的中文博客</span>
         </div>
         <div class="w-full text-center">
+          <NuxtLink to="https://beian.miit.gov.cn">
+            闽ICP备 2023011626号-3
+          </NuxtLink>
+        </div>
+        <div class="w-full text-center">
           <span
             >Copyright © 2022 - {{ year }} |
             <NuxtLink to="https://github.com/zh-blogs">
-              <i class="ri-github-fill" /> 中文博客列表导航项目
+              <i class="ri-github-fill" /> 集博栈（原中文博客列表导航）
             </NuxtLink>
           </span>
         </div>
