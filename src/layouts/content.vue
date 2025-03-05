48576abce1ebe3160ef5d24c2e06232d5dc75c3e
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-05 22:21:39 +0800

    feat: add sidebar component and content layout, reset the default layout

---
+++ src/layouts/content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HeaderItem />
+    <div class="mx-auto grid h-96 max-w-7xl grid-cols-[220px_1fr] gap-5">
+      <SideBar />
+      <div class="">
+        <slot />
+      </div>
+    </div>
+    <FooterItem />
+  </div>
+</template>
